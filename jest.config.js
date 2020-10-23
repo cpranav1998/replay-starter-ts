147b1594190a7377ede5b0aabb2d64a7ea3675ff
cpranav1998 <pranav720@gmail.com>
2020-10-22 22:18:42 -0400

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  preset: "ts-jest/presets/js-with-ts",
+  testEnvironment: "node",
+  testMatch: ["**/__tests__/**/*.test.ts"],
+  transformIgnorePatterns: ["/node_modules/(?!@replay/).+\\.js$"],
+  globals: { ASSET_NAMES: {} },
+};
